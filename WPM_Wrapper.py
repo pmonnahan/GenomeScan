8c7921f1b95dfd7a2686ece58fbbbc2052db6182
pmonnahan@gmail.com <pmonnahan@gmail.com>
2017-01-05 23:06:33 -0600

    Many changes to wrapper and wpm.py.  replaced missingness from wpm to number of individuals to downsample each population to remove effects of sample size on metrics.  Also added catch to wrapper to skip vcf parsing if recode files exist.

--- WPM_Wrapper.py
+++ WPM_Wrapper.py
@@ -4,11 +4,12 @@ import os
 import subprocess
 import argparse
 import pandas
+import math
 
 # create variables that can be entered in the command line
 parser = argparse.ArgumentParser()
 parser.add_argument('-PF', type=str, metavar='Individual_key', required=True, help='path to csv file containing information on what samples to include')
-parser.add_argument('-V', type=str, metavar='vcf_directory', required=True, default="-99", help='directory to vcf files to be split and converted to table')
+parser.add_argument('-V', type=str, metavar='vcf_directory', required=False, default="-99", help='directory to vcf files to be split and converted to table')
 parser.add_argument('-WS', type=str, metavar='Window_Size_BP', required=False, default='10000', help='size of windows in terms of bp; for geo window_type')
 parser.add_argument('-MS', type=str, metavar='Minimum_SNPs', required=False, default='10', help='Minimum number of SNPs in a window')
 parser.add_argument('-DP', type=str, metavar='Minimum_Depth', required=False, default='10', help='minimum average depth per individual')
@@ -24,25 +25,44 @@ POP_file = pandas.read_csv(args.PF, header=0)
 POP_names = list(POP_file.columns.values)[1:]
 sample_names = list(POP_file['Samples'])
 
+# Determine number of individuals to downsample all populations to
+min_ind = min([sum(list(POP_file[pop])) for pop in POP_names])
+sampind = int(math.ceil(min_ind * (1.0 - args.M)))
+if sampind == min_ind and args.M != 0.0:
+    sampind = min_ind - 1
 
 if args.O.endswith("/") is False:
     args.O = args.O + "/"
 if args.V != "-99" and args.V.endswith("/") is False:
     args.V = args.V + "/"
-
 if os.path.exists(args.O) is False:
     os.mkdir(args.O)
+
+# Determine if the recoded vcf files already exist and if so, set VCF_Parse to False
+VCF_Parse = True
 if os.path.exists(args.O + "Within-Population-Metrics/") is False:
     os.mkdir(args.O + "Within-Population-Metrics/")
+else:
+    existing_files = []
+    for file in os.listdir(args.O + "Within-Population-Metrics/"):
+        if file.endswith('.table.recode.txt'):
+            existing_files.append(file.split('.')[0])
+    if set(POP_names).issuperset(set(existing_files)) is True:
+        VCF_Parse = False
+        print("Recoded vcf files already exist.  Using pre-existing files and skipping vcf parsing")
+    # Look for '.recode' table files corresponding to POP_names...if they exist...skip all but wpm step and print message to output.
+
 if os.path.exists(args.O + "Within-Population-Metrics/OandE/") is False:
     os.mkdir(args.O + "Within-Population-Metrics/OandE/")
 
+# Main loop to cycle over populations in PF file
 for pop in POP_names:
     include_index = list(POP_file[pop])
     group1 = []
     sample_string1 = ""
     ss1 = ""
 
+    # Add samples to list for each population according to PF file
     for i, sample in enumerate(sample_names):
         if include_index[i] == 1:
             group1.append(sample)
@@ -66,117 +86,116 @@ for pop in POP_names:
 
     joblist = []
 
-    vcf_list = []
-    vcf_basenames = []
-    for file in os.listdir(args.V):
-        if file[-6:] == 'vcf.gz':
-            vcf_list.append(file)
-            vcf_basenames.append(file[:-7])
-        elif file[-3:] == 'vcf':
-            vcf_list.append(file)
-            vcf_basenames.append(file[:-4])
-
-
 # SPLIT VCF, CONVERT TO TABLES, REMOVE VCF
-    for v, vcf in enumerate(vcf_list):
+    if VCF_Parse is True:
+        vcf_list = []
+        vcf_basenames = []
+        for file in os.listdir(args.V):
+            if file[-6:] == 'vcf.gz':
+                vcf_list.append(file)
+                vcf_basenames.append(file[:-7])
+            elif file[-3:] == 'vcf':
+                vcf_list.append(file)
+                vcf_basenames.append(file[:-4])
+        for v, vcf in enumerate(vcf_list):
 
-        shfile1 = open(pop + '.sh', 'w')
-        shfile1.write('#!/bin/bash\n' +
+            shfile1 = open(pop + '.sh', 'w')
+            shfile1.write('#!/bin/bash\n' +
+                          '#SBATCH -J ' + pop + '.sh' + '\n' +
+                          '#SBATCH -e ' + oande + pop + vcf + '.gatk.err' + '\n' +
+                          '#SBATCH -o ' + oande + pop + vcf + '.gatk.out' + '\n' +
+                          '#SBATCH -p nbi-medium\n' +
+                          '#SBATCH -n 1\n' +
+                          '#SBATCH -t 0-4:00\n' +
+                          '#SBATCH --mem=16000\n' +
+                          'source GATK-3.6.0\n' +
+                          'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + args.V + vcf + sample_string1 + ' -o ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf\n' +
+                          'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf --restrictAllelesTo BIALLELIC -env -o ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf\n' +
+                          'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T VariantsToTable -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf -F CHROM -F POS -F AC -F AN -F DP -GF GT -o ' + outdir + vcf_basenames[v] + '.' + pop + '_raw.table\n')
+
+
+            if args.K == 'false':
+                shfile1.write('rm ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf\n' +
+                              'rm ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf\n' +
+                              'rm ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf.idx\n' +
+                              'rm ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf.idx')
+                shfile1.close()
+            else:
+                shfile1.write('gzip ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf')
+                shfile1.close()
+
+
+            if args.P1 == 'false':  # send slurm job to NBI SLURM cluster
+                cmd1 = ('sbatch ' + pop + '.sh')
+                p1 = subprocess.Popen(cmd1, shell=True)
+                sts1 = os.waitpid(p1.pid, 0)[1]
+                joblist.append(p1.pid)
+
+            else:
+                file1 = open(pop + '.sh', 'r')
+                data1 = file1.read()
+                print(data1)
+
+            os.remove(pop + '.sh')
+
+    # CONCATENATE THE INDIVIDUAL SCAFFOLD TABLES TO A SINGLE TABLE IN THE PARENT DIRECTORY
+    # THEN, REMOVE THE DIRECTORY CONTAINING SCAFFOLD FILES
+
+        shfile3 = open(pop + '.sh', 'w')
+
+        shfile3.write('#!/bin/bash\n' +
                       '#SBATCH -J ' + pop + '.sh' + '\n' +
-                      '#SBATCH -e ' + oande + pop + vcf + '.gatk.err' + '\n' +
-                      '#SBATCH -o ' + oande + pop + vcf + '.gatk.out' + '\n' +
-                      '#SBATCH -p nbi-medium\n' +
+                      '#SBATCH -e ' + oande + pop + '.cat.err' + '\n' +
+                      '#SBATCH -o ' + oande + pop + '.cat.out' + '\n' +
+                      '#SBATCH -p nbi-long\n' +
                       '#SBATCH -n 1\n' +
-                      '#SBATCH -t 0-4:00\n' +
-                      '#SBATCH --mem=16000\n' +
-                      'source GATK-3.6.0\n' +
-                      'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + args.V + vcf + sample_string1 + ' -o ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf\n' +
-                      'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf --restrictAllelesTo BIALLELIC -env -o ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf\n' +
-                      'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T VariantsToTable -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf -F CHROM -F POS -F AC -F AN -F DP -GF GT -o ' + outdir + vcf_basenames[v] + '.' + pop + '_raw.table\n')
-
-
+                      '#SBATCH -t 0-12:00\n' +
+                      '#SBATCH --mem=32000\n' +
+                      'cat ' + outdir + '*' + pop + '_raw.table | tail -n+2 > ' + outdir1 + pop + '.table\n')
         if args.K == 'false':
-            shfile1.write('rm ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf\n' +
-                          'rm ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf\n' +
-                          'rm ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf.idx\n' +
-                          'rm ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf.idx')
-            shfile1.close()
-        else:
-            shfile1.write('gzip ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf')
-            shfile1.close()
+            shfile3.write('rm -r ' + outdir + '\n')
+        shfile3.close()
 
-
-        if args.P1 == 'false':  # send slurm job to NBI SLURM cluster
-            cmd1 = ('sbatch ' + pop + '.sh')
-            p1 = subprocess.Popen(cmd1, shell=True)
-            sts1 = os.waitpid(p1.pid, 0)[1]
-            joblist.append(p1.pid)
-
-        else:
-            file1 = open(pop + '.sh', 'r')
-            data1 = file1.read()
-            print(data1)
+        if args.P2 == 'false':
+            cmd3 = ('sbatch -d singleton ' + pop + '.sh')
+            p3 = subprocess.Popen(cmd3, shell=True)
+            sts3 = os.waitpid(p3.pid, 0)[1]
+        elif args.P2 == 'true':
+            file3 = open(pop + '.sh', 'r')
+            data3 = file3.read()
+            print(data3)
 
         os.remove(pop + '.sh')
 
-# CONCATENATE THE INDIVIDUAL SCAFFOLD TABLES TO A SINGLE TABLE IN THE PARENT DIRECTORY
-# THEN, REMOVE THE DIRECTORY CONTAINING SCAFFOLD FILES
+    # FORMAT TABLE FOR WPM FILE.
+    # not done
 
-    shfile3 = open(pop + '.sh', 'w')
+        shfile3 = open(pop + '.sh', 'w')
+        shfile3.write('#!/bin/bash\n' +
+                      '#SBATCH -J ' + pop + '.sh' + '\n' +
+                      '#SBATCH -e ' + oande + pop + '.recode012.err' + '\n' +
+                      '#SBATCH -o ' + oande + pop + '.recode012.out' + '\n' +
+                      '#SBATCH -p nbi-long\n' +
+                      '#SBATCH -n 1\n' +
+                      '#SBATCH -t 1-00:00\n' +
+                      '#SBATCH --mem=32000\n' +
+                      'source python-3.5.1\n' +
+                      'source env/bin/activate\n' +
+                      'python3 /usr/users/JIC_c1/monnahap/GenomeScan/recode012.py -i ' + outdir1 + pop + '.table -mf ' + str(1.0 - args.M) + ' -dp ' + args.DP + ' -o ' + outdir1 + '\n')
+        if args.K == 'false':
+            shfile3.write('rm ' + outdir1 + pop + '.table')
+        shfile3.close()
 
-    shfile3.write('#!/bin/bash\n' +
-                  '#SBATCH -J ' + pop + '.sh' + '\n' +
-                  '#SBATCH -e ' + oande + pop + '.cat.err' + '\n' +
-                  '#SBATCH -o ' + oande + pop + '.cat.out' + '\n' +
-                  '#SBATCH -p nbi-long\n' +
-                  '#SBATCH -n 1\n' +
-                  '#SBATCH -t 0-12:00\n' +
-                  '#SBATCH --mem=32000\n' +
-                  'cat ' + outdir + '*' + pop + '_raw.table | tail -n+2 > ' + outdir1 + pop + '.table\n')
-    if args.K == 'false':
-        shfile3.write('rm -r ' + outdir + '\n')
-    shfile3.close()
+        if args.P2 == 'false':
+            cmd3 = ('sbatch -d singleton ' + pop + '.sh')
+            p3 = subprocess.Popen(cmd3, shell=True)
+            sts3 = os.waitpid(p3.pid, 0)[1]
+        elif args.P2 == 'true':
+            file3 = open(pop + '.sh', 'r')
+            data3 = file3.read()
+            print(data3)
 
-    if args.P2 == 'false':
-        cmd3 = ('sbatch -d singleton ' + pop + '.sh')
-        p3 = subprocess.Popen(cmd3, shell=True)
-        sts3 = os.waitpid(p3.pid, 0)[1]
-    elif args.P2 == 'true':
-        file3 = open(pop + '.sh', 'r')
-        data3 = file3.read()
-        print(data3)
-
-    os.remove(pop + '.sh')
-
-# FORMAT TABLE FOR WPM FILE.
-# not done
-
-    shfile3 = open(pop + '.sh', 'w')
-    shfile3.write('#!/bin/bash\n' +
-                  '#SBATCH -J ' + pop + '.sh' + '\n' +
-                  '#SBATCH -e ' + oande + pop + '.recode012.err' + '\n' +
-                  '#SBATCH -o ' + oande + pop + '.recode012.out' + '\n' +
-                  '#SBATCH -p nbi-long\n' +
-                  '#SBATCH -n 1\n' +
-                  '#SBATCH -t 1-00:00\n' +
-                  '#SBATCH --mem=32000\n' +
-                  'source python-3.5.1\n' +
-                  'source env/bin/activate\n' +
-                  'python3 /usr/users/JIC_c1/monnahap/GenomeScan/recode012.py -i ' + outdir1 + pop + '.table -mf ' + str(1.0 - args.M) + ' -dp ' + args.DP + ' -o ' + outdir1 + '\n')
-    if args.K == 'false':
-        shfile3.write('rm ' + outdir1 + pop + '.table')
-    shfile3.close()
-
-    if args.P2 == 'false':
-        cmd3 = ('sbatch -d singleton ' + pop + '.sh')
-        p3 = subprocess.Popen(cmd3, shell=True)
-        sts3 = os.waitpid(p3.pid, 0)[1]
-    elif args.P2 == 'true':
-        file3 = open(pop + '.sh', 'r')
-        data3 = file3.read()
-        print(data3)
-
-    os.remove(pop + '.sh')
+        os.remove(pop + '.sh')
 
 # CALCULATE WITHIN POPULATION METRICS
 # not done
@@ -192,7 +211,7 @@ for pop in POP_names:
                   '#SBATCH --mem=32000\n' +
                   'source python-3.5.1\n' +
                   'source env/bin/activate\n' +
-                  'python3 /usr/users/JIC_c1/monnahap/GenomeScan/wpm.py -i ' + outdir1 + pop + '.table.recode.txt -pop ' + pop + ' -o ' + outdir1 + ' -m ' + str(args.M) + ' -ws ' + args.WS + ' -ms ' + args.MS + '\n')
+                  'python3 /usr/users/JIC_c1/monnahap/GenomeScan/wpm.py -i ' + outdir1 + pop + '.table.recode.txt -pop ' + pop + ' -o ' + outdir1 + ' -sampind ' + str(sampind) + ' -ws ' + args.WS + ' -ms ' + args.MS + '\n')
     # if args.K == 'false':
     #     shfile3.write('rm ' + outdir1 + pop + '.table.recode.txt')
     shfile3.close()
--- wpm.py
+++ wpm.py
@@ -2,13 +2,12 @@
 Input file custom: Filename should end with _XXX_raw.table.recode.txt, where XXX is three-letter population abbreviation"""
 
 import argparse
-import math
 import numpy
 
 # export PYTHONPATH="$PYTHONPATH:/Users/monnahap/Documents/Research/code/GenomeScan/"
 
-
-def calcwpm(input_file, output, popname="pop", missingness=0.1, window_size=50000, minimum_snps=2):
+# Substitute missingness for number of individuals to downsample to.  That way you can enter one number for all populations to be downsampled to.
+def calcwpm(input_file, output, popname="pop", sampind=5, window_size=50000, minimum_snps=2):
 
     snp_count = 0
     start = 0.0
@@ -17,7 +16,7 @@ def calcwpm(input_file, output, popname="pop", missingness=0.1, window_size=5000
     num_wind = 0
     outfile = output + popname + "_WPM.txt"
     out1 = open(outfile, 'w')
-    out1.write("pop\tscaff\tstart\tend\twin_size\tnum_snps\tavg_freq\tavg_Ehet\tThetaW\tPi\tThetaH\tThetaL\tD\tH\tE\n")
+    out1.write("pop\tploidy\tsampind\tscaff\tstart\tend\twin_size\tnum_snps\tavg_freq\tavg_Ehet\tThetaW\tPi\tThetaH\tThetaL\tD\tH\tE\n")
 
     with open(input_file, 'r') as infile:
         for i, line in enumerate(infile):
@@ -36,9 +35,6 @@ def calcwpm(input_file, output, popname="pop", missingness=0.1, window_size=5000
                 oldscaff = scaff
                 totind = len(line[5:])
                 ploidy = int(an) / totind
-                sampind = int(math.ceil(totind * (1.0 - missingness)))
-                if sampind == totind and missingness != 0.0:
-                    sampind = totind - 1
                 AN = int(sampind * ploidy)
                 n = float(AN)
                 p = []
@@ -100,9 +96,10 @@ def calcwpm(input_file, output, popname="pop", missingness=0.1, window_size=5000
                     D = (Pi - W) / (varPi_W**0.5)
                     H = (Pi - L) / (varPi_L**0.5)  # Normalized H according to Zeng 2006
                     E = (L - W) / (varL_W**0.5)
-
-                    out1.write(popname + '\t' + scaff + '\t' +
+                    out1.write(popname + '\t' +
                                str(ploidy) + '\t' +
+                               str(sampind) + '\t' +
+                               scaff + '\t' +
                                str(start) + '\t' +
                                str(end) + '\t' +
                                str(window_size) + '\t' +
@@ -127,18 +124,78 @@ def calcwpm(input_file, output, popname="pop", missingness=0.1, window_size=5000
 
                 if float(pos) > end:
                     while float(pos) > end:
-                        end += window_size / 2
+                        end += window_size
                     start = end - window_size
                 elif scaff != oldscaff:
                     oldscaff = scaff
 
                     start = 0.0
                     end = window_size
+
+                if int(pos) > start and int(pos) <= end and int(an) >= AN and scaff == oldscaff:
+                    snp_count += 1
+                    sgt = numpy.random.choice(gt, size=sampind, replace=False)
+                    sac = sum([int(x) for x in sgt])
+                    p1 = float(sac) / float(AN)
+                    p.append(p1)
+                    Ehet.append(p1 * (1 - p1))
+                    afs[sac] += 1
+                    AFS[sac] += 1
+
+        if snp_count >= minimum_snps:
+            Pi = 0.0
+            h = 0.0
+            L = 0.0
+            n = float(AN)
+            num_wind += 1
+            S = float(sum(afs[1:-1]))
+            W = S / aw
+            W2 = S * (S - 1) / ((aw**2) + bw)
+            for j in range(1, AN):
+
+                Pi += afs[j] * j * (AN - j)
+                h += afs[j] * (j**2)
+                L += afs[j] * j
+
+            Pi = 2 * Pi / (n * (n - 1))
+            h = 2 * h / (n * (n - 1))
+            L = L / (n - 1)
+
+            # When calculating variance of D, H, and E should we use W value for window or for entire genome.  Probably for window??
+
+            varPi_W = e1 * S + e2 * S * (S - 1)
+            varPi_L = (((n - 2) / (6 * n - 6)) * W) + (((18 * n**2 * (3 * n + 2) * bw) - (88 * n**3 + 9 * n**2 - 13 * n + 6)) / (9 * n * (n - 1)**2) * W2)
+            varL_W = (((n / (2 * n - 2)) - 1 / aw) * W) + ((a2 / aw**2 + (2 * (n / (n - 1))**2) * a2 - 2 * (n * a2 - n + 1) / ((n - 1) * aw) - (3 * n + 1) / (n - 1)) * W2)
+
+            D = (Pi - W) / (varPi_W**0.5)
+            H = (Pi - L) / (varPi_L**0.5)  # Normalized H according to Zeng 2006
+            E = (L - W) / (varL_W**0.5)
+
+            out1.write(popname + '\t' +
+                       str(ploidy) + '\t' +
+                       str(sampind) + '\t' +
+                       scaff + '\t' +
+                       str(start) + '\t' +
+                       str(end) + '\t' +
+                       str(window_size) + '\t' +
+                       str(snp_count) + '\t' +
+                       str(numpy.mean(p)) + '\t' +
+                       str(numpy.mean(Ehet)) + '\t' +
+                       str(W) + '\t' +
+                       str(Pi) + '\t' +
+                       str(h) + '\t' +
+                       str(L) + '\t' +
+                       str(D) + '\t' +
+                       str(H) + '\t' +
+                       str(E) + '\n')
+    Pi = 0.0
+    h = 0.0
+    L = 0.0
+    n = float(AN)
     S = float(sum(AFS[1:-1]))
     W = S / aw
     W2 = S * (S - 1) / ((aw**2) + bw)
     for j in range(1, AN):
-
         Pi += AFS[j] * j * (AN - j)
         h += AFS[j] * (j**2)
         L += AFS[j] * j
@@ -157,7 +214,7 @@ def calcwpm(input_file, output, popname="pop", missingness=0.1, window_size=5000
     H = (Pi - L) / (varPi_L**0.5)  # Normalized H according to Zeng 2006
     E = (L - W) / (varL_W**0.5)
 
-    out1.write(popname + str(ploidy) + '\tGenome\t' +
+    out1.write(popname + '\t' + str(ploidy) + '\t' + str(sampind) + '\tGenome\t' +
                str("-99") + '\t' +
                str("-99") + '\t' +
                str("-99") + '\t' +
@@ -183,10 +240,10 @@ if __name__ == '__main__':
     parser.add_argument('-i', type=str, metavar='input_file', required=True, help='input file created with recode012.py')
     parser.add_argument('-o', type=str, metavar='output_directory', required=True, help='Output Directory')
     parser.add_argument('-pop', type=str, metavar='population_name', required=True, help='Name of population')
-    parser.add_argument('-m', type=float, metavar='Missingness', required=False, default='0.1', help='Proportion of missing individuals allowed in each group')
+    parser.add_argument('-sampind', type=int, metavar='DownSampled_individuals', required=False, default='5', help='Number of individuals to downsample the data to')
     parser.add_argument('-ws', type=float, metavar='window_size', required=False, default='10000.0', help='Size of windows in bp')
     parser.add_argument('-ms', type=int, metavar='minimum_snps', required=False, default='2', help='minimum number of snps in a window')
 
     args = parser.parse_args()
 
-    j1, j2, j3, j4, j5, j6, j7, j8, j9 = calcwpm(args.i, args.o, args.pop, args.m, args.ws, args.ms)
+    j1, j2, j3, j4, j5, j6, j7, j8, j9 = calcwpm(args.i, args.o, args.pop, args.sampind, args.ws, args.ms)
