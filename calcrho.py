6a6bc31d56a1d450723e1c6484bbcfdec5086178
pmonnahan@gmail.com <pmonnahan@gmail.com>
2017-01-09 14:42:13 -0600

    fixed small bug in wpm for Var(TajD), continuing work on calcrho

--- calcrho.py
+++ calcrho.py
@@ -6,15 +6,17 @@ import numpy
 
 # export PYTHONPATH="$PYTHONPATH:/Users/monnahap/Documents/Research/code/GenomeScan/"
 
+# concatenated, recoded files with  sort <filename> -k 3,4
+
 # Substitute missingness for number of individuals to downsample to.  That way you can enter one number for all populations to be downsampled to.
-def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, minimum_snps=2):
+def calcrho(input_file, output, popnum, sampind=5, window_size=50000, minimum_snps=2):
 
     snp_count = 0
     start = 0.0
     end = window_size
     winexclcount = 0
     num_wind = 0
-    outfile = output + popname + "_RHO.txt"
+    outfile = output + "_RHO.txt"
     out1 = open(outfile, 'w')
     out1.write("scaff\tstart\tend\twin_size\tnum_snps\tsampind\tRho\n")
 
@@ -51,7 +53,9 @@ def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, min
             if int(pos) > start and int(pos) <= end and scaff == oldscaff:
                 if pos == old_pos: # Accruing information from multiple populations but same locus
                     Locus.append(line)
-                else: # Within current window but have moved on from previous locus
+                elif len(Locus) == popnum: # Within current window but have moved on from previous locus
+                    if snp_count == 0:
+                        max_ind = max([len(x) for x in Locus])
                     X_i = []
                     sss = 0.0
                     ssi = 0.0
@@ -64,7 +68,7 @@ def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, min
                     Tot_alleles = 0.0
                     for pop_site in Locus:
                         ploidy = pop_site[1]
-                        num_ind = len(pop_site[7:]) # THIS MUST BE CHANGED IF DOWNSAMPLING IS IMPLEMENTED
+                        num_ind = len(pop_site[7:])  # THIS MUST BE CHANGED IF DOWNSAMPLING IS IMPLEMENTED
                         x_i = sum([int(geno) for geno in pop_site[7:]]) / (num_ind * ploidy)
                         X_i.append(x_i)
                         s1 += num_ind
@@ -83,15 +87,26 @@ def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, min
 
                     num += (wd * sss) - (ww * ssi)
                     den += (wa * ssi)
+                    Num += (wd * sss) - (ww * ssi)
+                    Den += (wa * ssi)
 
                     # RESET SUMS OF SQUARES
                     Locus = []
                     Locus.append(line)
                     old_pos = pos
                     snp_count += 1
-                    sgt = numpy.random.choice(gt, size=sampind, replace=False)
-                    sac = sum([int(x) for x in sgt])
-                    p1 = float(sac) / float(AN)
+                    # sgt = numpy.random.choice(gt, size=sampind, replace=False)
+                    # sac = sum([int(x) for x in sgt])
+                    p1 = float(ac) / float(AN)
+                    p.append(p1)
+                else:
+                    Locus = []
+                    Locus.append(line)
+                    old_pos = pos
+                    snp_count += 1
+                    # sgt = numpy.random.choice(gt, size=sampind, replace=False)
+                    # sac = sum([int(x) for x in sgt])
+                    p1 = float(ac) / float(AN)
                     p.append(p1)
 
 
@@ -99,10 +114,12 @@ def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, min
 
                 n = float(AN)
 
-                if snp_count >= minimum_snps: # Use or exclude window from 
+                if snp_count >= minimum_snps:  # Use or exclude window from 
                     num_wind += 1
 
-                    # When calculating variance of D, H, and E should we use W value for window or for entire genome.  Probably for window??
+                    fac = num / den
+
+                    rho = fac / (1 + fac)
 
                     out1.write(scaff + '\t' +
                                str(start) + '\t' +
@@ -118,6 +135,8 @@ def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, min
 
                 snp_count = 0
                 p = []
+                num = 0.0
+                den = 0.0
 
                 if float(pos) > end:
                     while float(pos) > end:
@@ -169,8 +188,9 @@ def calcrho(input_file, output, popname="pop", sampind=5, window_size=50000, min
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i1', type=str, metavar='input_file1', required=True, help='input file created with recode012.py')
+    parser.add_argument('-i', type=str, metavar='input_file', required=True, help='input file created with recode012.py')
     parser.add_argument('-o', type=str, metavar='output_directory', required=True, help='Output Directory')
+    parser.add_argument('-popnum', type=str, metavar='number_of_populations', required=True, help='')
     parser.add_argument('-sampind', type=int, metavar='DownSampled_individuals', required=False, default='5', help='Number of individuals to downsample the data to')
     parser.add_argument('-ws', type=float, metavar='window_size', required=False, default='10000.0', help='Size of windows in bp')
     parser.add_argument('-ms', type=int, metavar='minimum_snps', required=False, default='2', help='minimum number of snps in a window')
--- wpm.py
+++ wpm.py
@@ -33,7 +33,7 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
                 out1 = open(outfile, 'w')
                 out1.write("pop\tploidy\tsampind\tscaff\tstart\tend\twin_size\tnum_snps\tavg_freq\tavg_Ehet\tThetaW\tPi\tThetaH\tThetaL\tD\tH\tE\n")
                 oldscaff = scaff
-                AN = int(sampind * ploidy)
+                AN = int(sampind * int(ploidy))
                 n = float(AN)
                 p = []
                 Ehet = []
@@ -46,23 +46,27 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
                     aw += 1.0 / float(j)  # a1 in Tajima 1989 and an in Zeng 2006
                     a2 += 1.0 / float(j**2)  # This is bn according to Zeng 2006
                     bw += 1.0 / float(j**2)
-                bw += 1.0 / float(n**2)  # This is the n+1 part
+                bw += 1.0 / float(n**2)  # This is the n+1 part from Zeng
                 b1 = (n + 1) / (3 * (n - 1))  # b1 through e2 are taken directly rom Tajima 1989
-                b2 = (2 * n**2 + n + 3) / (9 * n * (n - 1))
+                b2 = (2 * (n**2 + n + 3)) / (9 * (n * (n - 1)))
                 c1 = b1 - (1 / aw)
                 c2 = b2 - (n + 2) / (aw * n) + a2 / aw**2
                 e1 = c1 / aw
                 e2 = c2 / (aw**2 + a2)
 
+  #               my $e1 = (1/$a1)*(($n+1)/(3*($n-1)) - (1/$a1)) ;
+  # my $e2 = (1/($a1**2 + $a2))*((2*(($n**2) + $n + 3))/(9*$n*($n-1)) - ($n+2)/($n*$a1) + ($a2)/($a1**2)) ;
+
             if int(pos) > start and int(pos) <= end and int(an) >= AN and scaff == oldscaff:
                 snp_count += 1
-                sgt = numpy.random.choice(gt, size=sampind, replace=False)
-                sac = sum([int(x) for x in sgt])
-                p1 = float(sac) / float(AN)
-                p.append(p1)
-                Ehet.append(p1 * (1 - p1))
-                afs[sac] += 1
-                AFS[sac] += 1
+                if len(gt) >= sampind:
+                    sgt = numpy.random.choice(gt, size=sampind, replace=False)
+                    sac = sum([int(x) for x in sgt])
+                    p1 = float(sac) / float(AN)
+                    p.append(p1)
+                    Ehet.append(p1 * (1 - p1))
+                    afs[sac] += 1
+                    AFS[sac] += 1
 
             elif int(pos) > end or scaff != oldscaff:
                 Pi = 0.0
@@ -71,10 +75,11 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
                 n = float(AN)
 
                 if snp_count >= minimum_snps:
+
                     num_wind += 1
                     S = float(sum(afs[1:-1]))
                     W = S / aw
-                    W2 = S * (S - 1) / ((aw**2) + bw)
+                    W2 = S * (S - 1) / ((aw**2) + a2)
                     for j in range(1, AN):
 
                         Pi += afs[j] * j * (AN - j)
@@ -85,6 +90,7 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
                     h = 2 * h / (n * (n - 1))
                     L = L / (n - 1)
 
+
                     # When calculating variance of D, H, and E should we use W value for window or for entire genome.  Probably for window??
 
                     varPi_W = e1 * S + e2 * S * (S - 1)
@@ -94,6 +100,7 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
                     D = (Pi - W) / (varPi_W**0.5)
                     H = (Pi - L) / (varPi_L**0.5)  # Normalized H according to Zeng 2006
                     E = (L - W) / (varL_W**0.5)
+
                     out1.write(pop + '\t' +
                                str(ploidy) + '\t' +
                                str(sampind) + '\t' +
@@ -132,13 +139,14 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
 
                 if int(pos) > start and int(pos) <= end and int(an) >= AN and scaff == oldscaff:
                     snp_count += 1
-                    sgt = numpy.random.choice(gt, size=sampind, replace=False)
-                    sac = sum([int(x) for x in sgt])
-                    p1 = float(sac) / float(AN)
-                    p.append(p1)
-                    Ehet.append(p1 * (1 - p1))
-                    afs[sac] += 1
-                    AFS[sac] += 1
+                    if len(gt) >= sampind:
+                        sgt = numpy.random.choice(gt, size=sampind, replace=False)
+                        sac = sum([int(x) for x in sgt])
+                        p1 = float(sac) / float(AN)
+                        p.append(p1)
+                        Ehet.append(p1 * (1 - p1))
+                        afs[sac] += 1
+                        AFS[sac] += 1
 
         if snp_count >= minimum_snps:
             Pi = 0.0
@@ -148,7 +156,7 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
             num_wind += 1
             S = float(sum(afs[1:-1]))
             W = S / aw
-            W2 = S * (S - 1) / ((aw**2) + bw)
+            W2 = S * (S - 1) / ((aw**2) + a2)
             for j in range(1, AN):
 
                 Pi += afs[j] * j * (AN - j)
@@ -192,7 +200,7 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
     n = float(AN)
     S = float(sum(AFS[1:-1]))
     W = S / aw
-    W2 = S * (S - 1) / ((aw**2) + bw)
+    W2 = S * (S - 1) / ((aw**2) + a2)
     for j in range(1, AN):
         Pi += AFS[j] * j * (AN - j)
         h += AFS[j] * (j**2)
@@ -205,9 +213,11 @@ def calcwpm(input_file, output, sampind=5, window_size=50000, minimum_snps=2):
     # When calculating variance of D, H, and E should we use W value for window or for entire genome.  Probably for window??
 
     varPi_W = e1 * S + e2 * S * (S - 1)
-    varPi_L = (((n - 2) / (6 * n - 6)) * W) + (((18 * n**2 * (3 * n + 2) * bw) - (88 * n**3 + 9 * n**2 - 13 * n + 6)) / (9 * n * (n - 1)**2) * W2)
+    varPi_L1 = (((n - 2) / (6 * n - 6)) * W)
+    varPi_L2 = (((18 * n**2 * (3 * n + 2) * bw) - (88 * n**3 + 9 * n**2 - 13 * n + 6)) / (9 * n * (n - 1)**2) * W2)
     varL_W = (((n / (2 * n - 2)) - 1 / aw) * W) + ((a2 / aw**2 + (2 * (n / (n - 1))**2) * a2 - 2 * (n * a2 - n + 1) / ((n - 1) * aw) - (3 * n + 1) / (n - 1)) * W2)
 
+    varPi_L = varPi_L1 + varPi_L2
     D = (Pi - W) / (varPi_W**0.5)
     H = (Pi - L) / (varPi_L**0.5)  # Normalized H according to Zeng 2006
     E = (L - W) / (varL_W**0.5)
