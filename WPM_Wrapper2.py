53449fe156a4af0c7cd6a9c6ac6737b761ddcc93
Patrick Monnahan <pmonnahan@gmail.com>
2017-01-18 11:37:28 +0000

    Fixes many errors in Wrapper2 and bpm upon adding linter to sublime

--- WPM_Wrapper2.py
+++ WPM_Wrapper2.py
@@ -16,9 +16,10 @@ import math
 
 # create variables that can be entered in the command line
 
+
 class PopGen:
 
-    def __init__(self,WorkingDir):
+    def __init__(self, WorkingDir):
         if WorkingDir.endswith("/") is False:
             WorkingDir += "/"
         if os.path.exists(WorkingDir) is False:
@@ -55,7 +56,7 @@ class PopGen:
         popname = str(popname)
         if popname in self.pops:
             self.pops.remove(popname)
-            self.samps.pop(popname,None)
+            self.samps.pop(popname, None)
             self.samp_nums.pop(popname, None)
             # Recalculate min_ind
             min_ind = min([self.samp_nums[pop] for pop in self.pops])
@@ -79,85 +80,81 @@ class PopGen:
 
         for pop in self.pops:
             # Add samples to list for each population according to PF file
-            group1 = []
             sample_string1 = ""
-            ss1 = ""
             for samp in self.samps[pop]:
                 sample_string1 += " -sn " + samp
-
             joblist = []
 
-        # SPLIT VCF, CONVERT TO TABLES, REMOVE VCF
-            if VCF_Parse is True:
-                vcf_list = []
-                vcf_basenames = []
-                for file in os.listdir(vcf_dir):
-                    if file[-6:] == 'vcf.gz':
-                        vcf_list.append(file)
-                        vcf_basenames.append(file[:-7])
-                    elif file[-3:] == 'vcf':
-                        vcf_list.append(file)
-                        vcf_basenames.append(file[:-4])
-                for v, vcf in enumerate(vcf_list):
-                    # Select single population and biallelic SNPs for each scaffold and convert to variants table
-                    shfile1 = open(pop + '.sh', 'w')
-                    shfile1.write('#!/bin/bash\n' +
-                                  '#SBATCH -J ' + pop + '.sh' + '\n' +
-                                  '#SBATCH -e ' + oande + pop + vcf + '.gatk.err' + '\n' +
-                                  '#SBATCH -o ' + oande + pop + vcf + '.gatk.out' + '\n' +
-                                  '#SBATCH -p nbi-medium\n' +
-                                  '#SBATCH -n 1\n' +
-                                  '#SBATCH -t 0-4:00\n' +
-                                  '#SBATCH --mem=' + mem + '\n' +
-                                  'source GATK-3.6.0\n' +
-                                  'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R ' + ref_path + ' -V ' + vcf_dir + vcf + sample_string1 + ' -o ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf\n' +
-                                  'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf --restrictAllelesTo BIALLELIC -env -o ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf\n' +
-                                  'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T VariantsToTable -R /nbi/group-data/JIC/Levi-Yant/Lyrata_ref/alygenomes.fasta -V ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf -F CHROM -F POS -F AC -F AN -F DP -GF GT -o ' + outdir + vcf_basenames[v] + '.' + pop + '_raw.table\n'
-                                  'gzip ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf')
-                    shfile1.close()
+    # SPLIT VCF, CONVERT TO TABLES, REMOVE VCF
+            vcf_list = []
+            vcf_basenames = []
+            for file in os.listdir(vcf_dir):
+                if file[-6:] == 'vcf.gz':
+                    vcf_list.append(file)
+                    vcf_basenames.append(file[:-7])
+                elif file[-3:] == 'vcf':
+                    vcf_list.append(file)
+                    vcf_basenames.append(file[:-4])
+            for v, vcf in enumerate(vcf_list):
+                # Select single population and biallelic SNPs for each scaffold and convert to variants table
+                shfile1 = open(pop + '.sh', 'w')
+                shfile1.write('#!/bin/bash\n' +
+                              '#SBATCH -J ' + pop + '.sh' + '\n' +
+                              '#SBATCH -e ' + self.oande + pop + vcf + '.gatk.err' + '\n' +
+                              '#SBATCH -o ' + self.oande + pop + vcf + '.gatk.out' + '\n' +
+                              '#SBATCH -p nbi-medium\n' +
+                              '#SBATCH -n 1\n' +
+                              '#SBATCH -t 0-4:00\n' +
+                              '#SBATCH --mem=' + mem + '\n' +
+                              'source GATK-3.6.0\n' +
+                              'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R ' + ref_path + ' -V ' + vcf_dir + vcf + sample_string1 + ' -o ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf\n' +
+                              'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T SelectVariants -R ' + ref_path + ' -V ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf --restrictAllelesTo BIALLELIC -env -o ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf\n' +
+                              'java -Xmx16g -jar /nbi/software/testing/GATK/3.6.0/src/GenomeAnalysisTK.jar -T VariantsToTable -R ' + ref_path + ' -V ' + outdir + vcf_basenames[v] + '.' + pop + '.bi.vcf -F CHROM -F POS -F AC -F AN -F DP -GF GT -o ' + outdir + vcf_basenames[v] + '.' + pop + '_raw.table\n'
+                              'gzip ' + outdir + vcf_basenames[v] + '.' + pop + '.vcf')
+                shfile1.close()
 
-                    if print1 is False:  # send slurm job to NBI SLURM cluster
-                        cmd1 = ('sbatch ' + pop + '.sh')
-                        p1 = subprocess.Popen(cmd1, shell=True)
-                        sts1 = os.waitpid(p1.pid, 0)[1]
-                        joblist.append(p1.pid)
+                if print1 is False:  # send slurm job to NBI SLURM cluster
+                    cmd1 = ('sbatch ' + pop + '.sh')
+                    p1 = subprocess.Popen(cmd1, shell=True)
+                    sts1 = os.waitpid(p1.pid, 0)[1]
+                    joblist.append(p1.pid)
 
-                    else:
-                        file1 = open(pop + '.sh', 'r')
-                        data1 = file1.read()
-                        print(data1)
+                else:
+                    file1 = open(pop + '.sh', 'r')
+                    data1 = file1.read()
+                    print(data1)
 
-                    os.remove(pop + '.sh')
+                os.remove(pop + '.sh')
 
-                    # combine all variants table for each scaffold within a population
-                    shfile3 = open(pop + '.sh', 'w')
+                # combine all variants table for each scaffold within a population
+                shfile3 = open(pop + '.sh', 'w')
 
-                    shfile3.write('#!/bin/bash\n' +
-                                  '#SBATCH -J ' + pop + '.sh' + '\n' +
-                                  '#SBATCH -e ' + self.oande + pop + '.cat.err' + '\n' +
-                                  '#SBATCH -o ' + self.oande + pop + '.cat.out' + '\n' +
-                                  '#SBATCH -p nbi-long\n' +
-                                  '#SBATCH -n 1\n' +
-                                  '#SBATCH -t 0-12:00\n' +
-                                  '#SBATCH --mem=32000\n' +
-                                  'cat ' + outdir + '*' + pop + '_raw.table | tail -n+2 > ' + outdir + pop + '.table\n')
-                    shfile3.close()
+                shfile3.write('#!/bin/bash\n' +
+                              '#SBATCH -J ' + pop + '.sh' + '\n' +
+                              '#SBATCH -e ' + self.oande + pop + '.cat.err' + '\n' +
+                              '#SBATCH -o ' + self.oande + pop + '.cat.out' + '\n' +
+                              '#SBATCH -p nbi-long\n' +
+                              '#SBATCH -n 1\n' +
+                              '#SBATCH -t 0-12:00\n' +
+                              '#SBATCH --mem=32000\n' +
+                              'cat ' + outdir + '*' + pop + '_raw.table | tail -n+2 > ' + outdir + pop + '.table\n')
+                shfile3.close()
 
-                    if print1 is False:
-                        cmd3 = ('sbatch -d singleton ' + pop + '.sh')
-                        p3 = subprocess.Popen(cmd3, shell=True)
-                        sts3 = os.waitpid(p3.pid, 0)[1]
-                    else:
-                        file3 = open(pop + '.sh', 'r')
-                        data3 = file3.read()
-                        print(data3)
+                if print1 is False:
+                    cmd3 = ('sbatch -d singleton ' + pop + '.sh')
+                    p3 = subprocess.Popen(cmd3, shell=True)
+                    sts3 = os.waitpid(p3.pid, 0)[1]
+                else:
+                    file3 = open(pop + '.sh', 'r')
+                    data3 = file3.read()
+                    print(data3)
 
-                    os.remove(pop + '.sh')
+                os.remove(pop + '.sh')
 
 
     def recode(self, min_avg_dp, missingness, print1=False):
 
-        sampind = int(math.ceil(min_ind * (1.0 - missingness)))
+        sampind = int(math.ceil(self.min_ind * (1.0 - missingness)))
         if sampind == self.min_ind and missingness != 0.0:
             sampind = self.min_ind - 1
         self.samp_ind = sampind
@@ -174,10 +171,9 @@ class PopGen:
                 print("Recoded vcf files already exist.  Delete folder or change parameters")
             # Look for '.recode' table files corresponding to POP_names...if they exist...skip all but wpm step and print message to output.
 
-
             else:
                 for pop in self.pops:
-                # FORMAT TABLE FOR WPM FILE.
+                    # FORMAT TABLE FOR WPM FILE.
 
                     shfile3 = open(pop + '.sh', 'w')
                     shfile3.write('#!/bin/bash\n' +
@@ -206,13 +202,15 @@ class PopGen:
 
     # CALCULATE WITHIN POPULATION METRICS
     def calcwpm(self, recode_dir, window_size, min_snps, print1=False, mem=16000):
-        summary = open(outdir1 + "WPMInputSummary.txt", 'w')
+
+        if recode_dir.endswith("/") is False:
+            recode_dir += "/"
+
+        summary = open(recode_dir + "WPMInputSummary.txt", 'w')
         summary.write('VCF Directory = ' + self.vcf_dir + '\n' +
                       'Window size in bp = ' + window_size + '\n' +
                       "Minimum number of SNPs per window  = " + min_snps + "\n")
         summary.close()
-        if recode_dir.endswith("/") is False:
-            recode_dir += "/"
 
         for pop in self.pops:
             shfile3 = open(pop + '.sh', 'w')
@@ -252,20 +250,19 @@ class PopGen:
                       '#SBATCH -p nbi-long\n' +
                       '#SBATCH -n 1\n' +
                       '#SBATCH -t 1-00:00\n' +
-                      '#SBATCH --mem=' +str(mem) + '\n' +
+                      '#SBATCH --mem=' + str(mem) + '\n' +
                       'source python-3.5.1\n' +
                       'source env/bin/activate\n' +
-                      'python3 /usr/users/JIC_c1/monnahap/GenomeScan/bpm.py -i1 ' + recode_dir + pop1 + '.table.recode.txt -i1 ' + recode_dir + pop2 + '.table.recode.txt -o ' + recode_dir + ' -ws ' + str(window_size) + ' -ms ' + str(min_snps) + '\n')
+                      'python3 /usr/users/JIC_c1/monnahap/GenomeScan/bpm.py -i1 ' + recode_dir + pop1 + '.table.recode.txt -i1 ' + recode_dir + pop2 + '.table.recode.txt -o ' + recode_dir + ' -ws ' + str(window_size) + ' -ms ' + str(minimum_snps) + '\n')
         shfile3.close()
 
         if print1 is False:
-            cmd3 = ('sbatch -d singleton ' + pop + '.sh')
+            cmd3 = ('sbatch -d singleton ' + pop1 + 'v' + pop2 + '.sh')
             p3 = subprocess.Popen(cmd3, shell=True)
             sts3 = os.waitpid(p3.pid, 0)[1]
         else:
-            file3 = open(pop + '.sh', 'r')
+            file3 = open(pop1 + 'v' + pop2 + '.sh', 'r')
             data3 = file3.read()
             print(data3)
 
-        os.remove(pop + '.sh')
-
+        os.remove(pop1 + 'v' + pop2 + '.sh')
--- bpm.py
+++ bpm.py
@@ -10,7 +10,7 @@ import numpy
 
 
 # Substitute missingness for number of individuals to downsample to.  That way you can enter one number for all populations to be downsampled to.
-def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps):
+def calcPairwiseBPM(input_file1, input_file2, output, outname, window_size, minimum_snps):
 
     def calcRho(locus_list):
         locus = []
@@ -24,15 +24,13 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
         X_i = []
         sss = 0.0
         ssi = 0.0
-        ssg = 0.0
         s1 = 0.0
         s2 = 0.0
-        ni =[]
+        ni = []
         ploidy_list = []
         for pop_site in locus:
             ploidy = int(pop_site[1])
             num_ind = len(pop_site[7:])  # THIS MUST BE CHANGED IF DOWNSAMPLING IS IMPLEMENTED
-            
             x_i = sum([float(geno) for geno in pop_site[7:]]) / (num_ind * ploidy)
             ni.append(num_ind)
             X_i.append(x_i)
@@ -40,13 +38,12 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
             s1 += num_ind
             s2 += num_ind**2
             for ind in pop_site[7:]:
-                x_g = float(ind)/float(ploidy)
-                ssi += ((x_g - x_i)**2)*float(ploidy)
+                x_g = float(ind) / float(ploidy)
+                ssi += ((x_g - x_i)**2) * float(ploidy)
         X_bar = numpy.mean(X_i)
         wa = s1 - (s2 / s1)
         wd = s1 - len(locus)
         ww = len(locus) - 1
-        sst = 0.0
         for i, x in enumerate(X_i):
             sss += ((x - X_bar)**2) * ni[i] * ploidy_list[i]
         num = (wd * sss) - (ww * ssi)
@@ -78,27 +75,27 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
             n_i.append(nnn)
             p_bar_i.append(ac / an)
             h = 0.0
-            if ploidy == 4.0
+            if ploidy == 4.0:
                 for ind in pop_site[7:]:
                     if float(ind) == 1.0 or float(ind) == 3.0:
                         h += 0.5
                     if float(ind) == 2.0:
                         h += 0.667
-            if ploidy == 2.0
-                for ind in pop_site[7:]
+            if ploidy == 2.0:
+                for ind in pop_site[7:]:
                     if float(ind) == 2.0:
                         h += 1.0
 
             h_i.append(h / nnn)
 
-        p_bar = sum([j*n_i[i] for i,j in enumerate(p_bar_i)]) / (r * n_bar)
-        h_bar = sum([j*n_i[i] for i,j in enumerate(p_bar_i)]) / (r * n_bar)
+        p_bar = sum([k * n_i[i] for i, k in enumerate(p_bar_i)]) / (r * n_bar)
+        h_bar = sum([k * n_i[i] for i, k in enumerate(p_bar_i)]) / (r * n_bar)
 
         var_p = 0.0
-        for i,p in enumerate(p_bar_i):
+        for i, p in enumerate(p_bar_i):
             var_p += (n_i[i] * (p - p_bar)**2) / ((r - 1) * n_bar)
 
-        a = (n_bar * n_C) * (var_p - ((1.0/(n_bar -1.0)) * ((p_bar * (1.0 - p_bar)) - (((r - 1.0) / r) * var_p) - (0.25 * h_bar))))
+        a = (n_bar * n_C) * (var_p - ((1.0 / (n_bar - 1.0)) * ((p_bar * (1.0 - p_bar)) - (((r - 1.0) / r) * var_p) - (0.25 * h_bar))))
         b = (n_bar / (n_bar - 1.0)) * ((p_bar * (1.0 - p_bar)) - (((r - 1.0) / r) * var_p) - (((2.0 * (n_bar - 1.0)) / (4.0 * n_bar)) * h_bar))
         c = 0.5 * h_bar
         d = (a + b + c)
@@ -108,7 +105,7 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
 
     def calcDxy(locus_info):
         locus = []
-        for l in locus_list:
+        for l in locus_info:
             try:
                 locus.append(l.remove("-9"))
             except ValueError:
@@ -130,12 +127,12 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
 
 
     # Concatenate input files and sort them
-    in1 = open(input_file1,'r').readlines()
-    in2 = open(input_file2,'r').readlines()
+    in1 = open(input_file1, 'r').readlines()
+    in2 = open(input_file2, 'r').readlines()
     in1 = [j.split("\t") for j in in1]
     in2 = [j.split("\t") for j in in2]
     data = in1 + in2
-    data = sorted(data, key=lambda k: (int(k[2].split("_")[1]), int(k[3]))) # Sorts by scaffold then position
+    data = sorted(data, key=lambda k: (int(k[2].split("_")[1]), int(k[3])))  # Sorts by scaffold then position
 
 
     snp_count = 0
@@ -144,12 +141,11 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
     end = window_size
     winexclcount = 0
     num_wind = 0
-    infile = input_file.split("/")[-1]
-    outfile = output + infile.replace('.txt','_RHO.txt')
+    outfile = output + outname + '_BPM.txt'
     out1 = open(outfile, 'w')
     out1.write("pop1\tpop2\tscaff\tstart\tend\twin_size\tnum_snps\tRho\tFst\tdxy\n")
 
-    # To downsample or not to downsample?? 
+    # To downsample or not to downsample??
     # Cat files prior running script and sort by scaffold and position
     # Does not handle missing data currently
     for i, line in enumerate(data):
@@ -160,14 +156,13 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
 
         pop, ploidy, scaff, pos, ac, an, dp = line[:7]
         pos = float(pos)
-        gt = line[7:]
 
         if i % 100000 == 0:
             print(i)
         if i == 0:
             pop1 = pop
             old_pos = pos
-            Locus = [] # Hold information of single locus across populations
+            Locus = []  # Hold information of single locus across populations
             oldscaff = scaff
             Fst = [0.0, 0.0]  # Fst[0] is numerator for genome, [1] is denominator for genome
             Rho = [0.0, 0.0]  # Rho[0] is numerator for genome, [1] is denominator for genome
@@ -177,12 +172,12 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
             Dxy = 0.0
 
         if pos > start and pos <= end and scaff == oldscaff:
-            if pos == old_pos: # Accruing information from multiple populations but same locus
+            if pos == old_pos:  # Accruing information from multiple populations but same locus
                 Locus.append(line)
                 if Snp_count == 0.0:
                     pop2 = pop
                     assert pop1 != pop2
-            elif len(Locus) == popnum: # Within current window but have moved on from previous locus
+            elif len(Locus) == 2:  # Within current window but have moved on from previous locus
                 new_rho = calcRho(Locus)
                 new_fst = calcFst(Locus)
                 dxy += calcDxy(Locus)
@@ -196,7 +191,7 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
                 Locus = []
                 Locus.append(line)
                 old_pos = pos
-                
+
             else:
                 Locus = []
                 Locus.append(line)
@@ -204,7 +199,7 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
 
 
         elif int(pos) > end or scaff != oldscaff:
-            if len(Locus) == popnum: # Current snp is onto next window, but must do calculation for previous locus before moving on
+            if len(Locus) == 2:  # Current snp is onto next window, but must do calculation for previous locus before moving on
                 snp_count += 1
                 n, d, j = calcRho(Locus)
                 a, b, c = calcFst(Locus)
@@ -216,7 +211,7 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
                 Rho = [sum(x) for x in zip(Rho, new_rho)]
 
             if snp_count >= minimum_snps:  # Use or exclude window from
-                Snp_count += snp_count 
+                Snp_count += snp_count
                 num_wind += 1
                 fst = fst[0] / fst[1]
                 fac = rho[0] / rho[1]
@@ -236,7 +231,6 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
                 winexclcount += 1
 
             snp_count = 0
-            p = []
             fst = [0.0, 0.0]  # Fst[0] is numerator for window, [1] is denominator for window
             rho = [0.0, 0.0]  # Rho[0] is numerator for window, [1] is denominator for window
             dxy = 0.0
@@ -256,9 +250,9 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
                 Locus.append(line)
                 old_pos = pos
 
-    if len(Locus) == popnum:  # Final window calculations
+    if len(Locus) == 2:  # Final window calculations
         snp_count += 1
-        n, d = calcRholocus(Locus)
+        n, d = calcRho(Locus)
         a, b, c = calcFst(Locus)
         dxy += calcDxy(Locus)
         Dxy += dxy
@@ -267,7 +261,7 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
         Fst = [sum(x) for x in zip(Fst, new_fst)]
         Rho = [sum(x) for x in zip(Rho, new_rho)]
 
-    if snp_count >= minimum_snps:  # Use or exclude window from 
+    if snp_count >= minimum_snps:  # Use or exclude window from
         num_wind += 1
         fst = fst[0] / fst[1]
         fac = rho[0] / rho[1]
@@ -276,13 +270,13 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
         dxy = dxy / snp_count
 
         out1.write(pop1 + '\t' + pop2 + '\t' + scaff + '\t' +
-                       str(start) + '\t' +
-                       str(end) + '\t' +
-                       str(window_size) + '\t' +
-                       str(snp_count) + '\t' +
-                       str(rho) + '\t' +
-                       str(fst) + '\t' +
-                       str(dxy) + '\n')
+                   str(start) + '\t' +
+                   str(end) + '\t' +
+                   str(window_size) + '\t' +
+                   str(snp_count) + '\t' +
+                   str(rho) + '\t' +
+                   str(fst) + '\t' +
+                   str(dxy) + '\n')
 
     else:
         winexclcount += 1
@@ -292,13 +286,13 @@ def calcPairwiseBPM(input_file1, input_file2, output, window_size, minimum_snps)
     Fst_G = Fst[0] / Fst[1]
     Dxy = Dxy / Snp_count
     out1.write(pop1 + '\t' + pop2 + '\t' + "Genome" + '\t' +
-         "-99" + '\t' +
-         "-99" + '\t' +
-         str(window_size) + '\t' +
-         str(Snp_count) + '\t' +
-         str(rho_G) + '\t' +
-         str(Fst_G) + '\t' +
-         str(Dxy) + '\n')
+               "-99" + '\t' +
+               "-99" + '\t' +
+               str(window_size) + '\t' +
+               str(Snp_count) + '\t' +
+               str(rho_G) + '\t' +
+               str(Fst_G) + '\t' +
+               str(Dxy) + '\n')
 
     out1.close()
 
@@ -311,9 +305,10 @@ if __name__ == '__main__':
     parser.add_argument('-i1', type=str, metavar='input_file1', required=True, help='input file created with recode012.py')
     parser.add_argument('-i2', type=str, metavar='input_file2', required=True, help='input file created with recode012.py')
     parser.add_argument('-o', type=str, metavar='output_directory', required=True, help='Output Directory')
+    parser.add_argument('-prefix', type=str, metavar='output_file_prefix', required=True, help='Prefix of output file...suggest Pop1vPop2')
     parser.add_argument('-ws', type=float, metavar='window_size', required=False, default='10000.0', help='Size of windows in bp')
     parser.add_argument('-ms', type=int, metavar='minimum_snps', required=False, default='2', help='minimum number of snps in a window')
 
     args = parser.parse_args()
 
-    j1, j2, j3 = calc(args.i1, args.i2, args.o, args.popnum, args.ws, args.ms)
+    j1, j2, j3 = calcPairwiseBPM(args.i1, args.i2, args.o, args.prefix, args.ws, args.ms)
