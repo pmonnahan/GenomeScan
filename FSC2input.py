20862779200f6535a3541cad3c4647886773ac4c
Patrick Monnahan <pmonnahan@gmail.com>
2017-02-02 11:16:24 +0000

    FSC2input: Fixes inputs for sbatch submission

--- FSC2input.py
+++ FSC2input.py
@@ -14,7 +14,7 @@ from functools import reduce
 # Add within population varP...could just use average SSI
 
 
-def generateFSC2input(input_file, output, outname, numpops, window_size, minimum_snps, num_bootstraps):
+def generateFSC2input(input_file, output, outname, numpops, window_size, num_bootstraps):
 
 
 
@@ -78,6 +78,7 @@ def generateFSC2input(input_file, output, outname, numpops, window_size, minimum
                 states_i.append([jj for jj in range(0, pop + 1)])
             states = list(itertools.product(*states_i))
             num_states = len(states)
+            dsfs = [0 for z in range(0, num_states + 1)]
             for rep in range(0, num_bootstraps):
                 exec("out%d = open('%s%s.rep%d_DSFS.obs', 'w')" % (rep + 1, output, outname, rep + 1), globals())
                 exec("out%d.write('1 observations.  No. of demes and sample sizes are on next line')" % (rep + 1), globals())
@@ -85,6 +86,7 @@ def generateFSC2input(input_file, output, outname, numpops, window_size, minimum
                 DSFS[str(rep)] = [0 for z in range(0, num_states + 1)]
                 exec("out%d.write('%s')" % (rep + 1, string), globals())
                 exec('out%d.write("""\n""")' % (rep + 1), globals())
+            out.write(str())
 
         if pos == old_pos:  # Accruing information from multiple populations but same locus
             Locus.append(line)
@@ -101,6 +103,7 @@ def generateFSC2input(input_file, output, outname, numpops, window_size, minimum
             list_pos = 0
             for jj, aa in enumerate(ac_i):
                     list_pos += aa * reduce(mul, [len(x) for x in states_i[jj + 1:]], 1)
+            dsfs[list_pos] += 1
             for j, rep in enumerate(wind_counts): 
                 for samp_num in range(0, rep[wind_num]):  # write site according to random number of times the window in which this site resides was chosen
                     DSFS[str(j)][list_pos] += 1
@@ -119,6 +122,7 @@ def generateFSC2input(input_file, output, outname, numpops, window_size, minimum
             old_pos = pos
 
     for state in range(num_states):
+        out.write()
         for rep in range(0, num_bootstraps):
             entry = DSFS[str(rep)][state]
             entry = str(entry) + "\t"
@@ -137,9 +141,9 @@ if __name__ == '__main__':  # Used to run code from command line
     parser.add_argument('-o', type=str, metavar='output_directory', required=True, help='Output Directory')
     parser.add_argument('-np', type=int, metavar='number_of_populations', required=True)
     parser.add_argument('-prefix', type=str, metavar='output_file_prefix', required=True, help='Name indicating populations in input file')
-    parser.add_argument('-ws', type=float, metavar='window_size', required=False, default='10000.0', help='Size of windows in bp')
-    parser.add_argument('-ms', type=int, metavar='minimum_snps', required=False, default='2', help='minimum number of snps in a window')
+    parser.add_argument('-ws', type=float, metavar='window_size', required=False, default='10000.0', help='Size of windows in bp; used for bootstrapping')
+    parser.add_argument('-bs', type=int, metavar='bootstrap_reps', required=False, default='5', help='number of bootstrap replicate datasets to generate')
 
     args = parser.parse_args()
 
-    j1, j2, j3 = generateFSC2input(args.i, args.o, args.prefix, args.ws, args.ms)
+    j1, j2 = generateFSC2input(args.i, args.o, args.np, args.prefix, args.ws, args.bs)
